d55e8281206f633953a7bc06cdb79120a3997cfb
giljihun <accc45@outlook.com>
2025-07-13 19:56:40 +0900

    Initial Commit

---
+++ KonglishSampleApp/KonglishSampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  KonglishSampleAppApp.swift
+//  KonglishSampleApp
+//
+//  Created by 길지훈 on 7/13/25.
+//
+
+import SwiftUI
+
+@main
+struct KonglishSampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
